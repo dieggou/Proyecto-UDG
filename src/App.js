34fc1fd248a9b7d8ff571ce490101a74983a543f
dieggou <diego123chavez123@gmail.com>
2021-09-03 22:02:01 -0500

    subiendo los primeros archivos

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import foto1 from './img/PC01.jpg'
+import foto2 from './img/PC02.jpg'
+import foto3 from './img/PC03.jpg'
+
+const items = [
+  {
+    src: foto1,
+    altText: 'PC 1',
+    caption: 'PC 1'
+  },
+  {
+    src: foto2,
+    altText: 'PC 2',
+    caption: 'PC 2'
+  },
+  {
+    src: foto3,
+    altText: 'PC 3',
+    caption: 'PC 3'
+  }
+];
+
+const App = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} width="100%" height="600px" />
+        <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+    </Carousel>
+  );
+}
+
+export default App;
